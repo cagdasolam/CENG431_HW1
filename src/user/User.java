d2bf49a766c7aaefdbcf3e1838dd187b75de3ab5
Çağdaş Olam <44732969+cagdisi@users.noreply.github.com>
2023-04-05 00:42:11 +0300

    add user

---
+++ src/user/User.java
@@ -0,0 +1,16 @@
+package user;
+
+import language.Language;
+import unit.Unit;
+
+public class User {
+
+	String userName;
+	String password;
+	Language selectedLanguage;
+	Unit currentUnit;
+	int streak;
+	int totalPoints;
+//	league
+
+}
